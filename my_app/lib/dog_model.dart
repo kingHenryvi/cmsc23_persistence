a54a81cf133efa488bd3476c86531135de11047b
kingHenryvi <msalcantara4@up.edu.ph>
2023-04-05 13:01:23 +0800

    added all necessary files

---
+++ my_app/lib/dog_model.dart
@@ -0,0 +1,8 @@
+class Dog {
+  String name;
+  String age;
+  Dog(this.name, this.age);
+  get dogData {
+    return '${name} ${age}\n';
+  }
+}
---
+++ my_app/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+//import necessary packages
+import 'show_dogs.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  static const String _title = 'Dogs List';
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: const ShowDogsPage(),
+    );
+  }
+}
